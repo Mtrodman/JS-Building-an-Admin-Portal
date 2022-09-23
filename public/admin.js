c3593675e4f8da366aa52eee0edd7d89b353b48d
matt rodman <mrodman89@gmail.com>
2022-09-23 08:10:36 -0500

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,53 @@
 
-// Your Code Here
+async function getList() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let bookUnit = await response.json()
+
+    console.log(bookUnit)
+
+    bookUnit.forEach(bookAdmin)
+}
+
+
+function bookAdmin(book) {
+    //creating a div for each book
+    let bookEntry = document.createElement("div")
+    //calling and appending the book titles
+    let bookTitle = document.createTextNode(`${book.title}`)
+    bookEntry.append(bookTitle)
+    //making inputs
+    let bookInput = document.createElement("input")
+    bookInput.setAttribute('type', 'number')
+    bookInput.value = `${book.quantity}`
+    bookEntry.append(bookInput)
+    //SUBMIT BUTTONS
+    let submit = document.createElement("button");
+    submit.setAttribute('id', 'submitButton');
+    let text = document.createTextNode("Submit");
+    submit.appendChild(text);
+
+    submit.addEventListener('click', patchIt)
+    async function patchIt() {
+        let response = await fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                "id": book.id,
+                "quantity": `${bookInput.value}`
+            })
+        });
+
+        await response.json()
+        alert(`${book.quantity}`)
+    }
+    bookEntry.append(submit)
+    //appending div
+    document.body.append(bookEntry)
+
+}
+
+getList()
+
